3d40768eb17ac3625626725ffc09643ecfa46c29
liuzhongsheng <996674366@qq.com>
2016-03-24 15:45:47 +0800

    删除无用文件

--- ThinkPHP_3.2.3/Contacts.list.api.php
+++
@@ -1,7 +0,0 @@
-<?php
-$this->checklogin(); 
-$list = C('CONTACTS_CONFIG');
-if(empty($list)){
-	$this -> error(10208);
-}
-$this -> success($list);
--- ThinkPHP_3.2.3/Meeting.recording.api.php
+++
@@ -1,9 +0,0 @@
-<?php
-$this->checklogin();
-$pagenum    = I('post.pagenum',1,'intval');
-$page    = I('post.page',10,'intval');
-$where = array(
-	'status' => 1,
-);
-$list = M('recording')->where($where)->field('id,title,thnum')->limit(($page-1)*$pagenum, $pagenum)->select();
-$this -> success($list);
