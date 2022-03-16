261630ce32651c9dd4732bd36ac14043b3e670cd
wsh3166Sir <51227820+wsh3166Sir@users.noreply.github.com>
2022-03-16 22:53:32 +0800

    test2

--- index.php
+++ index.php
@@ -16,9 +16,12 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
 
-// 定义应用目录323232
+// 定义应用目录  
 define('APP_PATH','./Application/');
 // 引入ThinkPHP入口文件
 require './ThinkPHP_3.2.3/ThinkPHP.php';
 
 // 亲^_^ 后面不需要任何代码了 就是如此简单
+
+
+//改更新下代码了，慢慢重构
